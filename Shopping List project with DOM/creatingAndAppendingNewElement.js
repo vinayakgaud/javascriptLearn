8ac6fd2a927f9d9e778b19a2af2b2f054d8f94c3
Vinayak <vinayak.gaud@gmail.com>
2023-10-28 00:30:07 +0530

    replacing element

--- Shopping List project with DOM/creatingAndAppendingNewElement.js
+++ Shopping List project with DOM/creatingAndAppendingNewElement.js
@@ -58,21 +58,23 @@ function createNewItem(item){
     li.appendChild(button);
     document.querySelector('.items').appendChild(li);
 }
+
 function createButton(classes){
     const button = document.createElement('button');
     button.className = classes;
     const icon = createIcon('fa-solid fa-xmark');
     button.appendChild(icon);
     return button;
-};
+}
+
 function createIcon(classes){
     const icon = document.createElement('i');
     icon.className = classes;
     return icon;
-};
+}
 
 function createText(item){
     const text = document.createTextNode(item);
     return text;
-};
+}
 createNewItem('Custard');
